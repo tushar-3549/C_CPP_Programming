845afa19c72471fd855185faf48044f46822b8a7
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2022-10-29 18:58:40 +0600

    Update Lab 5.c

--- Compiler Design Lab Task/Lab 5.c
+++ Compiler Design Lab Task/Lab 5.c
@@ -9,6 +9,7 @@ int main()
     int len = strlen(s);
     if((s[0] >= 'A' && s[0] <= 'Z') || (s[0] >= 'a' && s[0] <= 'z') || s[0] == '_')
     {
+        flag = 1;
         for(int i = 1; i < len; i++)
         {
             if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || s[i] == '_')
